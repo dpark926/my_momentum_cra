b70eff25e0305d89f7c928de6997ffd8d842d251
Daniel Park <dpeace926@gmail.com>
2019-03-14 19:07:40 -0400

    almost

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import { Router } from "react-router-dom";
 import Crypto from "./components/Crypto";
 import Stocks from "./components/Stocks";
 import TopNews from "./components/TopNews";
@@ -20,31 +21,33 @@ class App extends Component {
     const { weatherCityModalOpen, cryptoModalOpen } = this.state;
 
     return (
-      <div
-        className="inconsolata flex bg-black white absolute overflow-hidden col-12"
-        style={{ height: "100vh" }}
-      >
-        <div className="flex-auto">
-          <TimeDate />
-          <Weather />
-          <div className="flex">
-            <TopNews />
-            <Stocks />
-          </div>
-        </div>
-        <Crypto toggleCryptoModal={this.toggleCryptoModal} />
-        {cryptoModalOpen && (
-          <div className="absolute col-12" style={{ height: "100%" }}>
-            <div>
-              <form>
-                <label>BTC</label>
-                <input type="text" name="btc" />
-                <input type="submit" value="Submit" />
-              </form>
+      <Router>
+        <div
+          className="inconsolata flex bg-black white absolute overflow-hidden col-12"
+          style={{ height: "100vh" }}
+        >
+          <div className="flex-auto">
+            <TimeDate />
+            <Weather />
+            <div className="flex">
+              <TopNews />
+              <Stocks />
             </div>
           </div>
-        )}
-      </div>
+          <Crypto toggleCryptoModal={this.toggleCryptoModal} />
+          {cryptoModalOpen && (
+            <div className="absolute col-12" style={{ height: "100%" }}>
+              <div>
+                <form>
+                  <label>BTC</label>
+                  <input type="text" name="btc" />
+                  <input type="submit" value="Submit" />
+                </form>
+              </div>
+            </div>
+          )}
+        </div>
+      </Router>
     );
   }
 }
